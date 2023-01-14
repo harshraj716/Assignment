b2565606387943e84ba5658ce297e11985aa64dd
Harsh Raj <71925921+harshraj716@users.noreply.github.com>
2023-01-14 10:53:48 +0530

    Create ArrayObject.js

---
+++ ArrayObject.js
@@ -0,0 +1,4 @@
+let arr = [{name: "Orange", value: 1}, {name: "Grapes", value: 2},
+      {name: "Apple", value: 3}];
+      document.getElementById("idPrint").innerHTML = JSON.stringify(arr,
+      null, 4);
